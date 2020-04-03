e7419cdd5e5c40ca6191c69c501e68b5bb35fbd1
KCGallagher <gallagherkit@outlook.com>
2020-04-03 23:57:32 +0100

    Documented all functions in orbits

--- orbits.py
+++ orbits.py
@@ -28,46 +28,33 @@ vcos, vsin = greek_v * cos, greek_v * sin
 
 # Positions of massize bodies
 def solar_pos(t=0):
+    """Position of sun in 3-dimensions at time t
+
+    Note that z coordinate is fixed to 0, 
+    so movement is constrained to xy plane in this exact solution
+    """
     return np.array([-solar_rad * np.cos(omega * t), -solar_rad * np.sin(omega * t), 0])
 
 
 def planet_pos(t=0):
+    """Position of planet in 3-dimensions at time t
+
+    Note that z coordinate is fixed to 0, 
+    so movement is constrained to xy plane in this exact solution"""
     return np.array([planet_rad * np.cos(omega * t), planet_rad * np.sin(omega * t), 0])
 
 
 # assuming massive bodies do not deviate from xy plane
 
 
-# DEFINED FUNCTIONS IN THE ROTATING FRAME
-# def rot_acceleration(position, velocity, t):
-#     # factors defined for convenience
-#     solar_dr3 = np.linalg.norm(solar_pos(0) - position) ** 3  # should this be zero?
-#     planet_dr3 = np.linalg.norm(planet_pos(0) - position) ** 3
-
-#     virtual_force_x = position[0] * omega ** 2 + 2 * omega * velocity[1]
-#     virtual_force_y = position[1] * omega ** 2 - 2 * omega * velocity[0]
-#     virtual_force_z = 0  # change this!!
-
-#     return (
-#         -G
-#         * (
-#             M_S * (position[0] + solar_pos(0)[0]) / solar_dr3
-#             + M_P * (position[0] - planet_pos(0)[0]) / planet_dr3
-#         )
-#         + virtual_force_x,
-#         -G * (M_S * position[1] / solar_dr3 + M_P * position[1] / planet_dr3)
-#         + virtual_force_y,
-#         -G * (M_S * position[2] / solar_dr3 + M_P * position[2] / planet_dr3)
-#         + virtual_force_z,
-#     )
-
-
 def rot_derivatives(t, y):
+    """Gives derivative of each term of y at time t in the rotating frame
+
+    y should be of the form (x_pos, y_pos, z_pos, x_vel, y_vel, z_vel)
+    """
+
     position, velocity = np.array(y[0:3]), np.array(y[3:6])
 
-    print(position)
-    print(velocity)
-    print(y)
     solar_dr3 = np.linalg.norm(solar_pos(0) - position) ** 3  # should this be zero?
     planet_dr3 = np.linalg.norm(planet_pos(0) - position) ** 3
 
@@ -92,6 +79,13 @@ def rot_derivatives(t, y):
 
 
 def rotating_frame(y0_rot=(rcos, rsin, 0, 0, 0, 0)):
+    """Gives position and vlocity of asteroids in the rotating frame
+
+    Uses scipy solve_ivp method with LSODA, taking an input in the form
+    of (x_pos, y_pos, z_pos, x_vel, y_vel, z_vel) for the initial state.
+    This has a default value of the equillibrium position if not given
+    """
+
     return integrate.solve_ivp(
         fun=rot_derivatives,
         method="LSODA",  ###on adams advice
@@ -103,6 +97,7 @@ def rotating_frame(y0_rot=(rcos, rsin, 0, 0, 0, 0)):
 
 # DEFINED FUNCTIONS IN THE STATIONARY FRAME
 def stat_acceleration(position, t):
+    """Gives acceleration of asteroids in stationary frame at a given position and time"""
     # factors defined for convenience
     solar_dr3 = np.linalg.norm(solar_pos(t) - position) ** 3
     planet_dr3 = np.linalg.norm(planet_pos(t) - position) ** 3
@@ -123,11 +118,21 @@ def stat_acceleration(position, t):
 
 
 def stat_derivatives(t, y):
+    """Gives derivative of each term of y at time t in the stationary frame
+
+    y should be of the form (x_pos, y_pos, z_pos, x_vel, y_vel, z_vel)
+    """
     position, velocity = np.array(y[0:3]), np.array(y[3:6])
     return np.concatenate((velocity, stat_acceleration(position, t)))
 
 
 def stationary_frame(y0_stat=(rcos, rsin, 0, -vsin, vcos, 0)):
+    """Gives position and vlocity of asteroids in the stationary frame
+
+    Uses scipy solve_ivp method with LSODA, taking an input in the form
+    of (x_pos, y_pos, z_pos, x_vel, y_vel, z_vel) for the initial state.
+    This has a default value of the equillibrium state if not given.
+    """
     return integrate.solve_ivp(
         fun=stat_derivatives,
         method="LSODA",  ##on adams advice
