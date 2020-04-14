680bf5ac8b70de096e905d5793fe76ca1e22154f
KCGallagher <gallagherkit@outlook.com>
2020-04-14 15:17:01 +0100

    Sign implementation

--- wander.py
+++ wander.py
@@ -18,10 +18,11 @@ init_cond_stat = np.array(
 )
 
 greek_theta = np.arctan((R * math.sqrt(3) / 2) / (R / 2 - solar_rad))
+print(greek_theta)
 cos, sin = np.cos(greek_theta), np.sin(greek_theta)
 
 # Defined Functions
-def perturb(data, max_pertubation_size=0.01):
+def perturb(data, max_pertubation_size):
     """ Returns perturbed version of initial conditions array
 
     data is the initial conditions, submitted as a numpy array
@@ -34,7 +35,7 @@ def perturb(data, max_pertubation_size=0.01):
     )  # random betweeon -1 and 1
 
 
-def max_wander(max_pertubation_size=0.01, samples=3):
+def max_wander(max_pertubation_size, samples):
     """Returns maximum deviation over orbit in the rotating frame for given number of samples
 
     This calculates the distange from the initial point (not the lagrange point)
@@ -44,28 +45,37 @@ def max_wander(max_pertubation_size=0.01, samples=3):
     pertubation_size determies the relative magnitude of the perturbation as in perturb()
     Samples denotes the number of random pertubations sampled for gives perturbation size
     """
-    wander = np.zeros((samples, 3))  # size of pertubation; size of wander
+    output = np.zeros((samples, 3))  # size of pertubation; size of wander
     for n in range(samples):
         init_cond = perturb(init_cond_rot, max_pertubation_size)
         orbit = orbits.rotating_frame(init_cond)
         sample_wander = np.zeros((len(orbit.t)))
+
+        perturb_theta = np.arctan(
+            (init_cond[1] - init_cond_rot[1]) / (init_cond[0] - init_cond_rot[0])
+        )
+        sign = np.sign(init_cond[1] - init_cond_rot[1]) * np.sign(
+            init_cond[0] - init_cond_rot[0]
+        )
         for i in range(len(sample_wander)):
             sample_wander[i] = np.linalg.norm(
                 orbit.y[0:3, i]
                 - init_cond[0:3]
                 # - lagrange[0:3]
             )  # deviation in pos only
-        # wander[n, 0] = np.linalg.norm(
-        #     init_cond[0:3] - lagrange[0:3]
-        # )  # magnitude of pertubation
-        wander[n, 0] = np.linalg.norm((init_cond[0:3] - lagrange[0:3])) * np.cos(
-            greek_theta - np.arctan(init_cond[1] / init_cond[0])
+        output[n, 0] = (
+            np.linalg.norm((init_cond[0:3] - lagrange[0:3]))
+            * np.abs(np.cos(greek_theta - perturb_theta))
+            # * sign
         )  # radial component of pertubation
-        wander[n, 1] = np.linalg.norm((init_cond[0:3] - lagrange[0:3])) * np.sin(
-            greek_theta - np.arctan(init_cond[1] / init_cond[0])
+        print(np.cos(greek_theta - perturb_theta))
+        output[n, 1] = (
+            np.linalg.norm((init_cond[0:3] - lagrange[0:3]))
+            * np.abs(np.sin(greek_theta - perturb_theta))
+            # * sign
         )  # tangential component of pertubation
-        wander[n, 2] = np.max(sample_wander)  # size of wander
-    return wander
+        output[n, 2] = np.max(sample_wander)  # size of wander
+    return output
 
 
 def wander(pertubation, samples=1, pertubation_type="position"):
@@ -95,20 +105,26 @@ def wander(pertubation, samples=1, pertubation_type="position"):
     return np.max(wander_t)
 
 
-# wander_data = max_wander(max_pertubation_size=0.001, samples=100)
-# plt.plot(wander_data[:, 0], wander_data[:, 2], linestyle="None", marker="x")
-# plt.title("Wander against Pertubation size")
-# plt.ylabel("Wander")
-# plt.xlabel("Radial Pertubation Size")
-# # plt.savefig("wanderagainstradialpertubation.png")
-# plt.show()
+wander_data = max_wander(max_pertubation_size=0.001, samples=10)
+plt.plot(wander_data[:, 0], wander_data[:, 2], linestyle="None", marker="x")
+plt.plot(
+    wander_data[:, 0],
+    np.poly1d(np.polyfit(wander_data[:, 0], wander_data[:, 2], 1))(wander_data[:, 0]),
+    linewidth=0.5,
+)
+plt.plot(wander_data[:, 0], wander_data[:, 2], linestyle="None", marker="x")
+plt.title("Wander against Pertubation size")
+plt.ylabel("Wander /AU")
+plt.xlabel("Radial Pertubation Size /AU")
+plt.savefig("wanderagainstradialpertubation.png")
+plt.show()
 
-# plt.plot(wander_data[:, 1], wander_data[:, 2], linestyle="None", marker="x")
-# plt.title("Wander against Pertubation size")
-# plt.ylabel("Wander")
-# plt.xlabel("Tangential Pertubation Size")
-# # plt.savefig("wanderagainsttangentialpertubation.png")
-# plt.show()
+plt.plot(wander_data[:, 1], wander_data[:, 2], linestyle="None", marker="x")
+plt.title("Wander against Pertubation size")
+plt.ylabel("Wander /AU")
+plt.xlabel("Tangential Pertubation Size /AU")
+plt.savefig("wanderagainsttangentialpertubation.png")
+plt.show()
 
 
 # for i in range(3):
@@ -121,82 +137,81 @@ def wander(pertubation, samples=1, pertubation_type="position"):
 # need other way to define stability as other stable orbits away from lagrange point may form
 
 
+# # START OF MESH GRID PLOTS
+
+# grid_size = 0.04
+# sampling_points = 30
+# # make these smaller to increase the resolution
+# dx, dy = grid_size / sampling_points, grid_size / sampling_points
+
+# # generate 2 2d grids for the x & y bounds
+# y, x = np.mgrid[
+#     slice(-grid_size, grid_size + dy, dy), slice(-grid_size, grid_size + dx, dx)
+# ]
+
+# import time
+
+# z = np.zeros_like(x)
+# for i, j in np.ndindex(z.shape):
+#     start = time.time()
+#     z[i, j] = wander((x[i, j], y[i, j]), pertubation_type="velocity")
+#     end = time.time()
+#     print(str((i, j)) + " in time " + str(end - start) + " s")
+
+
+# # x and y are bounds, so z should be the value *inside* those bounds.
+# # Therefore, remove the last value from the z array.
+# z = z[:-1, :-1]
+
+# # pick the desired colormap
+# # cmap = plt.get_cmap("PiYG")
+
+# fig = plt.figure()
+# ax0 = fig.add_subplot()
+
+# im = ax0.pcolormesh(x, y, z, label="x")  # cmap=cmap)  # , norm=norm)
+# cbar = fig.colorbar(im)
+# cbar.ax.set_ylabel("Wander /AU")
+
+# ax0.set_title("Wander from starting point in the rotating frame")
+# ax0.set_xlabel("Deviation from Lagrange velocity in x direction (AU/year)")
+# ax0.set_ylabel("Deviation from Lagrange velocity in y direction (AU/year)")
+
+# plt.savefig("testvelocitymesh.png")
+# plt.show()
+
 # import matplotlib
 # from matplotlib.colors import BoundaryNorm
 # from matplotlib.ticker import MaxNLocator
 
+# fig = plt.figure()
+# ax1 = fig.add_subplot()
 
-grid_size = 0.04
-sampling_points = 30
-# make these smaller to increase the resolution
-dx, dy = grid_size / sampling_points, grid_size / sampling_points
+# levels = MaxNLocator(nbins=15).tick_values(z.min(), z.max())
 
-# generate 2 2d grids for the x & y bounds
-y, x = np.mgrid[
-    slice(-grid_size, grid_size + dy, dy), slice(-grid_size, grid_size + dx, dx)
-]
+# # pick the desired colormap, sensible levels, and define a normalization
+# # instance which takes data values and translates those into levels.#
 
-import time
-
-z = np.zeros_like(x)
-for i, j in np.ndindex(z.shape):
-    start = time.time()
-    z[i, j] = wander((x[i, j], y[i, j]), pertubation_type="velocity")
-    end = time.time()
-    print(str((i, j)) + " in time " + str(end - start) + " s")
-
-
-# x and y are bounds, so z should be the value *inside* those bounds.
-# Therefore, remove the last value from the z array.
-z = z[:-1, :-1]
-
-# pick the desired colormap
 # cmap = plt.get_cmap("PiYG")
+# norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
 
-fig = plt.figure()
-ax0 = fig.add_subplot()
+# # contours are *point* based plots, so convert our bound into point
+# # centers
+# cf = ax1.contourf(
+#     x[:-1, :-1] + dx / 2.0, y[:-1, :-1] + dy / 2.0, z, levels=levels, cmap=cmap
+# )
 
-im = ax0.pcolormesh(x, y, z, label="x")  # cmap=cmap)  # , norm=norm)
-cbar = fig.colorbar(im)
-cbar.ax.set_ylabel("Wander /AU")
+# cbar = fig.colorbar(cf, ax=ax1)
+# cbar.ax.set_ylabel("Wander /AU")
+# ax1.set_title("Wander from Starting Point in the Rotating Frame")
+# ax1.set_xlabel("Deviation from Lagrange velocity in x direction (AU/year)")
+# ax1.set_ylabel("Deviation from Lagrange velocity in y direction (AU/year)")
+# plt.savefig("testvelocitymeshb.png")
+# plt.show()
 
-ax0.set_title("Wander from starting point in the rotating frame")
-ax0.set_xlabel("Deviation from Lagrange velocity in x direction (AU/year)")
-ax0.set_ylabel("Deviation from Lagrange velocity in y direction (AU/year)")
-
-plt.savefig("testvelocitymesh.png")
-plt.show()
-
-import matplotlib
-from matplotlib.colors import BoundaryNorm
-from matplotlib.ticker import MaxNLocator
-
-fig = plt.figure()
-ax1 = fig.add_subplot()
-
-levels = MaxNLocator(nbins=15).tick_values(z.min(), z.max())
-
-# pick the desired colormap, sensible levels, and define a normalization
-# instance which takes data values and translates those into levels.#
-
-cmap = plt.get_cmap("PiYG")
-norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
-
-# contours are *point* based plots, so convert our bound into point
-# centers
-cf = ax1.contourf(
-    x[:-1, :-1] + dx / 2.0, y[:-1, :-1] + dy / 2.0, z, levels=levels, cmap=cmap
-)
-
-cbar = fig.colorbar(cf, ax=ax1)
-cbar.ax.set_ylabel("Wander /AU")
-ax1.set_title("Wander from Starting Point in the Rotating Frame")
-ax1.set_xlabel("Deviation from Lagrange velocity in x direction (AU/year)")
-ax1.set_ylabel("Deviation from Lagrange velocity in y direction (AU/year)")
-plt.savefig("testvelocitymeshb.png")
-plt.show()
 
 # STATIONARY FRAME
+
 # for i in range(2):
 #     orbit_sol2 = orbits.stationary_frame(perturb(init_cond_stat, 0.3))
 #     plt.plot(
